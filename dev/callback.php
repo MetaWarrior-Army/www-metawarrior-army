c27eb311d7e907c3183f77fc1b145da75ee189d1
nutherone <nutherone550@gmail.com>
2023-10-14 10:29:09 +0000

    login work

--- dev/callback.php
+++ dev/callback.php
@@ -1,10 +1,9 @@
 <?php
-# Start server session
 
 include '../php/mwa.php';
 include './config.php';
 
-
+# Start server session
 session_start();
 
 // session should already be stored
@@ -17,57 +16,65 @@ if(isset($_GET['code']) && isset($_GET['scope']) && isset($_GET['state']) && iss
 	if($hashed_secret == $re[1]){
 		echo "<p>Session Matches</p>";
 	}
-	else{
+	else{ // Redirect bad user
 		echo "<p>Session doesn't match</p>";
 		header("Location: /dev/login.php");
 	}
 }
-else{
+else{ // redirect to login
 	header("Location: /dev/login.php");
 }
 
-// Session is valid, now let's verify the code we received is legit. 
-$auth_str = base64_encode($OAUTH_CLIENT_ID.":".$OAUTH_CLIENT_SECRET);
-$header = ['Authorization: Basic '.$auth_str];
+// Check to see if we already have an access_token
+if(isset($_SESSION['access_token'])){
+	echo "<p>Access Token: ".$_SESSION['access_token']->access_token."</p>";
+	echo "<p>Expires In: ".$_SESSION['access_token']->expires_in."</p>";
+	echo "<p>ID Token: ".$_SESSION['access_token']->id_token."</p>";
+	echo "<p>Refresh Token: ".$_SESSION['access_token']->refresh_token."</p>";
+	echo "<p>Scope: ".$_SESSION['access_token']->scope."</p>";
+	echo "<p>Token Type: ".$_SESSION['access_token']->token_type."</p>";
+}
+else{
+	// Session is valid, now let's verify the code we received is legit. 
+	$auth_str = base64_encode($OAUTH_CLIENT_ID.":".$OAUTH_CLIENT_SECRET);
+	$header = ['Authorization: Basic '.$auth_str];
 
-var_dump($header);
+	$post = [
+		'client_id' => $OAUTH_CLIENT_ID,
+		'code' => $_GET['code'],
+		'grant_type' => 'authorization_code',
+		'redirect_uri' => 'https://www.metawarrior.army/dev/callback.php'
+	];
 
-$post = [
-	'client_id' => $OAUTH_CLIENT_ID,
-	//'client_secret' => $OAUTH_CLIENT_SECRET,
-	'code' => $_GET['code'],
-	'grant_type' => 'authorization_code',
-	'redirect_uri' => 'https://www.metawarrior.army/dev/callback.php'
-];
-	
+	$url = "https://auth.metawarrior.army/oauth2/token";
+	$crl = curl_init($url);
+	curl_setopt($crl, CURLOPT_HTTPHEADER,$header);
+	curl_setopt($crl, CURLOPT_POST,true);
+	curl_setopt($crl, CURLOPT_POSTFIELDS,$post);
+	curl_setopt($crl,CURLOPT_RETURNTRANSFER, true);
 
-$js_post = json_encode($post);
+	try {
+		$resp = json_decode(curl_exec($crl));
+	}
+	catch (Exception $e){
+		echo 'Caught exception: ', $e->getMessage();
+	}
 
-var_dump($js_post);
-$url = "https://auth.metawarrior.army/oauth2/token";
-$crl = curl_init($url);
-curl_setopt($crl, CURLOPT_HTTPHEADER,$header);
-curl_setopt($crl, CURLOPT_POST,true);
-curl_setopt($crl, CURLOPT_POSTFIELDS,$post);
-//rl_setopt($crl, CURLOPT_RETURNTRANSFER, true);
+	echo "<p>Access Token: ".$_SESSION['access_token']->access_token."</p>";
+	echo "<p>Expires In: ".$_SESSION['access_token']->expires_in."</p>";
+	echo "<p>ID Token: ".$_SESSION['access_token']->id_token."</p>";
+	echo "<p>Refresh Token: ".$_SESSION['access_token']->refresh_token."</p>";
+	echo "<p>Scope: ".$_SESSION['access_token']->scope."</p>";
+	echo "<p>Token Type: ".$_SESSION['access_token']->token_type."</p>";
 
-$resp = curl_exec($crl);
+}
 
-
-
-curl_close($crl);
-
-var_dump($resp);
-
-
-
-// Below is the HTML the user will interact with.
 ?>
 
-	
-	
-	
-</main>
+<html>
+<body>
+	<main>	
+	</main>
 
 </body>
 </html>
